62c38b20d9203d80bfe1f08ad1e9a4c50f8c4f3d
nathannprvt <nathan.cv.nguyen@gmail.com>
2025-04-14 15:00:51 -0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public void test() {
+        System.out.println("Test method");
+    }
+}
